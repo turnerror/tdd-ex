e0f4303a567851de782574afb466cfef4b998e5d
turnerror <christianturner@hotmail.co.uk>
2020-06-02 12:02:45 +0100

    added requirement 6

--- Tests/TestGreet.php
+++ Tests/TestGreet.php
@@ -28,9 +28,39 @@ class TestGreet extends TestCase
         $this->assertEquals("Hello, Jill and Jane.", $actual);
     }
 
-    public function testSuccess_anyXNames()
+    public function testSuccess_2namesUppercase()
+    {
+        $actual = greet(['JILL', 'JANE']);
+        $this->assertEquals("HELLO JILL AND JANE!", $actual);
+    }
+
+    public function testSuccess_lowerCaseNames()
     {
         $actual = greet(["Amy", "Brian", "Charlotte", "Jeff"]);
         $this->assertEquals("Hello, Amy, Brian, Charlotte, and Jeff.", $actual);
+    }
+
+    public function testSuccess_upperCaseNames()
+    {
+        $actual = greet(["AMY", "BRIAN", "CHARLOTTE", "JEFF"]);
+        $this->assertEquals("HELLO AMY, BRIAN, CHARLOTTE, AND JEFF!", $actual);
+    }
+
+    public function testSuccess_bothCaseNames1()
+    {
+        $actual = greet(["Amy", "Brian", "Charlotte", "Jeff", "AMY", "BRIAN", "CHARLOTTE", "JEFF"]);
+        $this->assertEquals("Hello, Amy, Brian, Charlotte, and Jeff. AND HELLO AMY, BRIAN, CHARLOTTE, AND JEFF!", $actual);
+    }
+
+    public function testSuccess_bothCaseNames2()
+    {
+        $actual = greet(["Amy", "BRIAN", "Charlotte", "Jeff", "ALEX"]);
+        $this->assertEquals("Hello, Amy, Charlotte, and Jeff. AND HELLO BRIAN AND ALEX!", $actual);
+    }
+
+    public function testSuccess_bothCaseNames3()
+    {
+        $actual = greet(["Amy", "BRIAN", "Charlotte", "Jeff"]);
+        $this->assertEquals("Hello, Amy, Charlotte, and Jeff. AND HELLO BRIAN!", $actual);
     }
 }
--- functions.php
+++ functions.php
@@ -2,25 +2,73 @@
 
 function greet($names){
 
-    if (is_array($names)){
-        if (count($names) == 2){
-            $msg = 'Hello, ' . $names[0] . ' and ' . $names[1] . '.';
-        } else {
-            $msg = 'Hello, ';
-            foreach ($names as $i=>$name){
-                if ($i == count($names) - 1) {
-                    $msg .= 'and ' . $name . '.';
-                } else {
-                    $msg .= $name . ', ';
-                }
+    if (is_array($names)) {
+        $lowercaseNames = [];
+        $uppercaseNames = [];
+        foreach ($names as $i => $name) {
+            if (strtoupper($name) === $name) {
+                $uppercaseNames[] = $names[$i];
+            } else {
+                $lowercaseNames[] = $names[$i];
             }
         }
+        $uppercaseMsg = '';
+        $lowercaseMsg = '';
 
-    } elseif (strtoupper($names) === $names) {
-        $msg = 'HELLO ' . $names . "!";
+        if (count($lowercaseNames) > 1){
+            $lowercaseMsg = 'Hello, ' . handleNamesArray($lowercaseNames) . '.';
+        } elseif (count($lowercaseNames) == 1){
+            $lowercaseMsg = handleName($lowercaseNames[0]);
+        }
+
+        if (count($uppercaseNames) > 1) {
+            $uppercaseMsg = 'HELLO ' . handleNamesArray($uppercaseNames) . '!';
+        } elseif (count($uppercaseNames) == 1){
+            $uppercaseMsg = handleName($uppercaseNames[0]);
+        }
+
+        if (!empty($lowercaseMsg) && (!empty($uppercaseMsg))){
+            $msg = $lowercaseMsg . ' AND ' . $uppercaseMsg;
+        } else {
+            $msg = $lowercaseMsg . $uppercaseMsg;
+        }
+
     } else {
-        $insert = $names ?? 'my friend';
-        $msg = 'Hello, ' . $insert . '.';
+        $msg = handleName($names);
     }
     return $msg;
 }
+
+function handleNamesArray(array $names): string {
+    $return = '';
+
+    if (count($names) == 2){
+        $return .= $names[0];
+        $return .= (strtoupper($names[0]) === $names[0]) ? ' AND ' : ' and ';
+        $return .= $names[1];
+
+    } else {
+        foreach ($names as $i => $name) {
+            if ($i == count($names) - 1) {
+                $return .= (strtoupper($name) === $name) ? 'AND ' : 'and ';
+                $return .= $name;
+            } else {
+                $return .= $name . ', ';
+            }
+        }
+    }
+
+    return $return;
+}
+
+function  handleName($name) {
+    if (strtoupper($name) === $name) {
+        $return = 'HELLO ' . $name . "!";
+    } else {
+        $insert = $name ?? 'my friend';
+        $return = 'Hello, ' . $insert . '.';
+    }
+
+    return $return;
+}
+
